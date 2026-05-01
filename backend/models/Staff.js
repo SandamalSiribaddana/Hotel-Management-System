de1ebf42c2489ea178152d8d5d9a2c7674e91d20
SayuniDeSilva <sayunidesilva20@gmail.com>
2026-05-01 21:34:39 +0530

    Create Staff.js

---
+++ backend/models/Staff.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const staffSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    salary: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Staff", staffSchema);
